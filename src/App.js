a2a686a92b1ad72ff3d038200917d9e8609bee87
JacobBerkey <jacobberkey@yahoo.com>
2021-10-11 17:08:23 -0400

    getAllSongs, createNewSoong and deleteSong functions in App.js completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            songs: []
+        }
+    }
+
+    componentDidMount() {
+        this.getAllSongs();
+    }
+
+    async getAllSongs() {
+        try {
+            let response = await axios.get('http://127.0.0.1:8000/music/');
+            this.setState({
+                songs: response.data
+            })
+        } catch (ex) {
+            console.log("API Error", ex)
+        }
+    }
+
+    createNewSong = async() => {
+        try {
+            await axios.post('http://127.0.0.1:8000/music/')
+        } catch (ex) {
+            console.log("API Create Song Error", ex)
+        }
+    }
+
+    deleteSong = async(songId) => {
+        try {
+            await axios.delete('http://127.0.0.1:8000/music/')
+        } catch (ex) {
+            console.log("API Delete Song Error", ex)
+        }
+    }
+
+    render() {
+        return ( <
+            div >
+            <
+            h1 > Hello World < /h1> <
+            /div>
+        );
+    }
 }
 
 export default App;
